78ba469ff908863b349693c72fc8a5d77b5a6cf6
p33yush <peeyushhanumante@gmail.com>
2026-02-08 12:05:49 +0530

    Express Server Setup done

---
+++ src/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+app.use(cors());           // Allows requests from your React frontend
+app.use(helmet());         // Adds security headers to protect your API
+app.use(morgan('dev'));    // Logs every request: "GET / 200 5ms"
+app.use(express.json());   // Parses JSON in request body (for POST/PUT requests)
+
+app.get('/', (req, res) => {
+    res.json({ message: 'MovieReservation API' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
